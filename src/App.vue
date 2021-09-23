6819e52718054e67095131dbbca9bc5cb33474e8
kazuma-0 <as8644459@gmail.com>
2021-09-23 23:03:45 +0530

    add css classes

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,18 @@
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
+body{
+    background-color: rgb(32, 29, 36) !important;
+    color: #FBF7F4;
+}
+.color-white{
+    color: #FBF7F4;
+}
+.color-purple{
+    background-color:#352D39 !important;
+}
+
+.pe{
+    cursor: pointer !important;
+}
 </style>
