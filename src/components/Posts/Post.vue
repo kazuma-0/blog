c5c825a3807b920f06342df77416dda5c78c7d2b
kazuma-0 <as8644459@gmail.com>
2021-09-23 23:06:00 +0530

    add functionality for saving posts

--- src/components/Posts/Post.vue
+++ src/components/Posts/Post.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div class="card color-purple color-white">
     <img
       :src="post.thumbnail"
       alt="error"
@@ -15,7 +15,7 @@
         {{ post.short_description }} ...
       </div>
 
-      <div class="tags d-flex">
+      <div class="tags d-flex" v-if="post.tags != null">
         <div
           class="tag me-2 rounded px-1 text-muted"
           v-for="tag in post.tags.split(',')"
---
+++ src/components/Posts/SavePost.vue
@@ -0,0 +1,87 @@
+<template>
+<div class="position-absolute top-50 start-50 translate-middle">
+    <div class="card m-390 p-4 position-relative">
+        <form @submit.prevent>
+            <div class="input-group mb-3 ">
+                <span class="input-group-text w-input">
+                    <i class="fas fa-heading"/>
+            </span>
+            <input type="text" required class="form-control" placeholder="Post title" v-model="title">
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text w-input">
+                    <i class="fas fa-info-circle"/>
+            </span>
+            <input type="text" required class="form-control" placeholder="Short Description" v-model="short_description">
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text w-input">
+                    <!-- <i class="fas fa-info-circle"/> -->
+                    <i class="fas fa-file-image" aria-hidden="true"></i>
+            </span>
+            <input type="text" class="form-control" placeholder="Thumbnail" v-model="thumbnail">
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text w-input">
+                    <!-- <i class="fas fa-info-circle"/> -->
+                  <i class="fas fa-tags"></i>
+            </span>
+            <input type="text" class="form-control" placeholder="Tags" v-model="tags">
+            </div>
+            <button class="form-control btn-success mb-3" @click="savePost">Save</button>
+            <div class="form-control btn-danger text-center user-select-none" @click="HideSaveEmit">Cancel</div>
+        </form>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    methods:{
+        HideSaveEmit(){
+            this.$emit('hide-save');
+        },
+        async savePost(){
+            if(this.thumbnail == ""){
+                this.thumbnail = null
+            }
+            const {data, error} = await this.$supabase.from('Posts')
+            .insert([{
+                title:this.title,
+                content:this.Content,
+                short_description:this.short_description,
+                thumbnail:this.thumbnail,
+                tags:this.tags
+            }])
+            if(error){
+                console.log(error)
+            }else{
+                console.log(data);
+            }
+        }
+    },
+    props:{
+        Content:String
+    },
+    data(){
+        return{
+            title:"",
+            short_description:"",
+            thumbnail: "",
+            tags:""
+        }
+    },
+    emits:['hide-save']
+}
+</script>
+
+<style scoped>
+.m-390{
+    min-width: 380px;
+    max-width: 390px;
+}
+.w-input{
+    width :40px;
+    text-align: center;
+}
+</style>
