39a16e113c519b0e8709a33cd52ef273cefff6ff
kazuma-0 <as8644459@gmail.com>
2021-09-23 11:53:03 +0530

    create NewPost.vue

---
+++ src/views/NewPost.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="min-vh-100 bg-dark">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-6"></div>
+                <div class="col-6"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+*{
+    outline: 1px red solid;
+}
+</style>
