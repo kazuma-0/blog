ea13c17390a29501398fafaefc291aa386537aa0
kazuma-0 <as8644459@gmail.com>
2021-09-23 23:04:11 +0530

    add new plugin

--- src/main.js
+++ src/main.js
@@ -9,10 +9,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@popperjs/core'
 import Alert from './components/Alert.vue'
 import Supabase from './plugins/subabase'
+import Markdown from 'vue3-markdown-it';
 
 const app = createApp(App)
 app.config.globalProperties.$supabase = Supabase;
 app.component('alert-view', Alert)
 app.use(store)
+app.use(Markdown)
 app.use(router)
 app.mount('#app')
