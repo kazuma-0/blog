b9a9eaa6ec0d664c9445274db7be115d37cf81c5
kazuma-0 <as8644459@gmail.com>
2021-09-26 18:01:56 +0530

    Add loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,9 @@
+<template>
+    <transition name="fade">
+    <div class="position-fixed top-50 start-50 translate-middle color-white">
+        <div class="spinner-grow" role="status">
+        <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+    </transition>
+</template>
