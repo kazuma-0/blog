2e85647712fbb0be07c6a41379c5cbae6b9853c3
kazuma-0 <as8644459@gmail.com>
2021-09-28 12:14:38 +0530

    remove markdown from global component

--- src/main.js
+++ src/main.js
@@ -9,13 +9,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@popperjs/core'
 import Alert from './components/Alert.vue'
 import Supabase from './plugins/subabase'
-import Markdown from 'vue3-markdown-it';
 import Loading from './components/Loading.vue'
 const app = createApp(App)
 app.config.globalProperties.$supabase = Supabase;
 app.component('alert-view', Alert)
 app.component('Loading', Loading)
 app.use(store)
-app.use(Markdown)
 app.use(router)
 app.mount('#app')
